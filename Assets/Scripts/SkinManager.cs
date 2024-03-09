96e884839243d44337fa1a5bb63a4ac5463800ce
ju583097 <olivieri.julian@aol.com>
2024-03-09 13:55:24 -0500

    updated skin manager
    character select now has the capablility to hold an array for the player names

--- Assets/Scripts/SkinManager.cs
+++ Assets/Scripts/SkinManager.cs
@@ -1,37 +1,55 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SkinManager : MonoBehaviour
 {
     public SpriteRenderer sr;
     public List<Sprite> skins = new List<Sprite>();
+    public string[] skinNames; 
     private int selectedSkin = 0;
     public GameObject playerskinPrefab;
+    public TextMeshProUGUI skinNameText;
+
+    private void Start()
+    {
+        UpdateSkinNameText();
+    }
+
+    private void UpdateSkinNameText()
+    {
+        
+        if (selectedSkin >= 0 && selectedSkin < skinNames.Length)
+        {
+            skinNameText.text = skinNames[selectedSkin];
+        }
+        else
+        {
+            Debug.LogWarning("Selected skin index is out of bounds!");
+        }
+    }
 
     public void NextOption()
     {
         selectedSkin = (selectedSkin + 1) % skins.Count;
         sr.sprite = skins[selectedSkin];
+        UpdateSkinNameText();
     }
 
     public void BackOption()
     {
         selectedSkin = (selectedSkin - 1 + skins.Count) % skins.Count;
         sr.sprite = skins[selectedSkin];
+        UpdateSkinNameText();
     }
 
     public void PlayGame()
     {
-        
         GameObject selectedSkinInstance = Instantiate(playerskinPrefab);
-
-       
 #if UNITY_EDITOR
         UnityEditor.PrefabUtility.SaveAsPrefabAsset(selectedSkinInstance, "Assets/Prefabs/SelectedSkin.prefab");
 #endif
-
-        
         SceneManager.LoadScene("LevelSelection");
     }
 }
