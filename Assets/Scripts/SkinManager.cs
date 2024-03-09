c01306a46a79c0b772b04c85a17b439f39bbca6b
ju583097 <olivieri.julian@aol.com>
2024-03-09 12:49:51 -0500

    updated skin manager
    updated skin manager to work in a build

--- Assets/Scripts/SkinManager.cs
+++ Assets/Scripts/SkinManager.cs
@@ -1,52 +1,37 @@
 using UnityEngine;
-using TMPro;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
-using UnityEditor;
-
 
 public class SkinManager : MonoBehaviour
 {
     public SpriteRenderer sr;
     public List<Sprite> skins = new List<Sprite>();
-    public List<string> skinNames = new List<string>();
     private int selectedSkin = 0;
-    public GameObject playerskin;
-    public TextMeshProUGUI skinNameText; 
-
-    private void Start()
-    {
-        UpdateSkinInfo();
-    }
+    public GameObject playerskinPrefab;
 
     public void NextOption()
     {
         selectedSkin = (selectedSkin + 1) % skins.Count;
-        UpdateSkinInfo();
+        sr.sprite = skins[selectedSkin];
     }
 
     public void BackOption()
     {
-        if (skins.Count == 0)
-        {
-            
-            return;
-        }
-
         selectedSkin = (selectedSkin - 1 + skins.Count) % skins.Count;
-        UpdateSkinInfo();
+        sr.sprite = skins[selectedSkin];
     }
 
     public void PlayGame()
     {
-        GameObject selectedSkinInstance = Instantiate(playerskin);
-        PrefabUtility.SaveAsPrefabAsset(selectedSkinInstance, "Assets/SelectedSkin.prefab");
-        SceneManager.LoadScene("LevelSelection");
-    }
+        
+        GameObject selectedSkinInstance = Instantiate(playerskinPrefab);
 
-    private void UpdateSkinInfo()
-    {
-        sr.sprite = skins[selectedSkin];
-        skinNameText.text = skinNames[selectedSkin]; 
+       
+#if UNITY_EDITOR
+        UnityEditor.PrefabUtility.SaveAsPrefabAsset(selectedSkinInstance, "Assets/Prefabs/SelectedSkin.prefab");
+#endif
+
+        
+        SceneManager.LoadScene("LevelSelection");
     }
 }
